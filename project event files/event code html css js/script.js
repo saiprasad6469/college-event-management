dd7df3188dc784e1beb7f4b97671a783365b1676
saiprasad6469 <saiprasadandugule@gmail.com>
2025-02-20 17:28:33 +0530

    Delete project event files directory

--- project event files/event code html css js/script.js
+++
@@ -1,9 +0,0 @@
-// script.js
-
-// Add click event to each register button
-document.querySelectorAll('.register-btn').forEach(button => {
-    button.addEventListener('click', () => {
-        const eventName = button.parentElement.querySelector('h3').innerText;
-        alert(You have registered for the ${eventName}! 🎉);
-    });
-});
